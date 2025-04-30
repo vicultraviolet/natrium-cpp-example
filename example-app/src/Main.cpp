c81200948d1b697b4e2c1b82ee379b45ef9de2d0
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2025-04-30 13:56:33 -0300

    Left entry point handling to Natrium Left entry point handling to Natrium Updated natrium-cpp

--- example-app/src/Main.cpp
+++ example-app/src/Main.cpp
@@ -3,32 +3,14 @@
 
 int main(int argc, char* argv[])
 {
-	using namespace ExampleApp;
-	try {
-		Na::Context context = Na::Context::Initialize();
+	Na::Context context = Na::Context::Initialize();
 
-		{
-			// performance is better with heap allocation, i profiled
-			std::unique_ptr<Game> game = std::make_unique<Game>();
-			game->run();
-		}
-
-		context.Shutdown();
-	} catch (const std::exception& e)
 	{
-		ExampleApp::g_Logger.fmt(Na::Fatal, "Unhandled exception reached main! {}", e.what());
-		return -1;
+		// performance is better with heap allocation, i profiled
+		std::unique_ptr<ExampleApp::Game> game = std::make_unique<ExampleApp::Game>();
+		game->run();
 	}
+
+	context.Shutdown();
 	return 0;
 }
-
-#if (defined(NA_PLATFORM_WINDOWS) && defined(NA_CONFIG_DIST))
-int APIENTRY WinMain(
-	HINSTANCE hInst,
-	HINSTANCE hInstPrev,
-	PSTR cmdline,
-	int cmdshow)
-{
-	return main(__argc, __argv);
-}
-#endif 
