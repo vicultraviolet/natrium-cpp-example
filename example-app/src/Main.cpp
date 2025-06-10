f07003a2ec51fb65826c8a3ff3d2be7df3ad739e
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2025-06-10 17:35:49 -0300

    Refactored application instantiation in Main.cpp
    Modified the `main` function to conditionally create an instance of `Na::Application` based on the `APP_HEAP` definition
    Updated `natrium-cpp`

--- example-app/src/Main.cpp
+++ example-app/src/Main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char* argv[])
 {
+#if !defined(APP_HEAP)
 	Na::Application app(
 		1280, 720, "Natrium Example", // window
 		"assets/engine/", "bin/shaders/",
@@ -10,5 +11,14 @@ int main(int argc, char* argv[])
 	);
 	app.create_layer<ExampleApp::MainLayer>();
 	app.run();
+#else
+	auto app = Na::UniqueRef<Na::Application>::Make(
+		1280, 720, "Natrium Example", // window
+		"assets/engine/", "bin/shaders/",
+		"renderer_settings.json"
+	);
+	app->create_layer<ExampleApp::MainLayer>();
+	app->run();
+#endif // APP_HEAP
 	return 0;
 }
